5680c2cf4f80066bc59cbc020f3c4ae33d7a8c97
sophiaharms <40673245+sophiaharms@users.noreply.github.com>
2020-09-20 13:09:25 -0400

    Create sph.js

---
+++ sph.js
@@ -0,0 +1,39 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
+
+ var timeOut = null;
+
+ $('.next').click(function(e,simulated){
+     if(!simulated){
+         clearTimeout(timeOut);
+      }
+ });
+
+ (function autoAdvance(){
+   $('.next').trigger('click',[true]);
+     timeOut = setTimeout(autoAdvance,5000);
+ })();
