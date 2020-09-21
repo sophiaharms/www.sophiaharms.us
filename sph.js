dea531a6612f348dbc15ca85fa5ba70a344fcccd
sophiaharms <40673245+sophiaharms@users.noreply.github.com>
2020-09-20 22:30:18 -0400

    Update sph.js

--- sph.js
+++ sph.js
@@ -12,13 +12,13 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  $(document).ready(function() {
+  var dots = document.getElementsByClassName("dot");
+   $(document).ready(function() {
   if (location.hash) {
     var hash = location.hash.slice(1);
     $(".dot").eq(hash).trigger('click'); 
   }
 });
-  var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1} 
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
